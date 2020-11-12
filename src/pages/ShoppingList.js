e053bc63ffa20c9b43785c05b271eb72504d020b
Kexin Liu <blue_star1022@sina.com>
2020-11-13 01:33:50 +0800

    Add ShoppingList page

--- src/pages/ShoppingList.js
+++ src/pages/ShoppingList.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { useData } from "../data/dataProvider"
 
 export default function ShoppingList() {
-  return <div></div>;
+  const { items } = useData()
+
+  return (
+    <div>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            <div>
+              <img className="product-image"
+                src={item.img_src}>
+              </img>
+              <h1>{item.name.toUpperCase()}</h1>
+              <h1>${item.price}/{item.metric}</h1>
+              <form>
+                <label for="quantity">Quantity:</label>
+                <input type="number" id="quantity" name="quantity" min="1" max="99" />
+                <input type="submit" value = "Add to cart" />
+              </form>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
