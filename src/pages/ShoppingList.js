9d60988fae98fbcda687a246561ca07660b5e500
Kexin Liu <blue_star1022@sina.com>
2020-11-14 02:26:57 +0800

    Edited item database stockinfo format and average function for products' prices

--- src/pages/ShoppingList.js
+++ src/pages/ShoppingList.js
@@ -3,18 +3,28 @@ import { useData } from "../data/dataProvider"
 
 export default function ShoppingList() {
   const { items } = useData()
+  function getAvePrice(stockInfo) {    
+    var sum = 0
+    for (var i = 0; i < stockInfo.length; i++) {
+      sum += stockInfo[i].price
+    }
+    var avg = sum/stockInfo.length
+    return avg
+  }
 
   return (
     <div>
+    
       <ul>
         {items.map(item => (
           <li key={item.id}>
             <div>
               <img className="product-image"
-                src={item.img_src}>
+                src={item.img_src}
+                alt="product">
               </img>
               <h1>{item.name.toUpperCase()}</h1>
-              <h1>${item.price}/{item.metric}</h1>
+              <h1>${getAvePrice(item.stockInfo)}/{item.metric}</h1>
               <form>
                 <label for="quantity">Quantity:</label>
                 <input type="number" id="quantity" name="quantity" min="1" max="99" />
