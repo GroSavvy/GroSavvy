cb6dd3aa34b60be8ee80db3e846f823348e81bfb
Kexin Liu <blue_star1022@sina.com>
2020-11-16 02:07:02 +0800

    add handler dealing with submit list and navigate to mylists page

--- src/pages/ShoppingList.js
+++ src/pages/ShoppingList.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { useData } from "../data/dataProvider";
+import { useNavigate } from "react-router-dom";
 
 export default function ShoppingList() {
   const { items, addNewList } = useData();
   const [list, setList] = useState({});
+  let navigate = useNavigate();
 
 
   const addItem = (e, item_id) => {
@@ -44,6 +46,13 @@ export default function ShoppingList() {
     return avg;
   }
 
+  const handleChange = (e) => {
+    submit(e, list)
+    navigate(`/mylists`);
+  };
+
+
+
   return (
     <div className="content">
       <div className="main">
@@ -54,8 +63,9 @@ export default function ShoppingList() {
                 <img className="img-thumbnail" src={item.img_src} alt={item.name}></img>
                 <h2>{item.name.toUpperCase()}</h2>
                 <span>
-                  Approx. <h1>${getAvePrice(item.stockInfo)}</h1>/{item.metric}
+                  Approx. <h1>${getAvePrice(item.stockInfo).toFixed(2)}</h1>/{item.metric}
                 </span>
+                
                 <form>
                   <label htmlFor="quantity">Quantity:</label>
                   <input
@@ -83,6 +93,7 @@ export default function ShoppingList() {
         <h2>Shopping Cart</h2>
         <div>
           {Object.keys(list).length == 0 ?
+          
             <div className="empty-cart" className="alert alert-info" role="alert">
               Cart is empty.
             </div>
@@ -105,7 +116,7 @@ export default function ShoppingList() {
                 <button
                   type="submit"
                   className="btn btn-primary"
-                  onClick={e => submit(e, list)}
+                  onClick={handleChange}
                 >
                   Create List
                 </button>
@@ -116,11 +127,9 @@ export default function ShoppingList() {
                 >
                   Clear List
                 </button>
-                
               </div>
             </div>
           }
-
         </div>
       </div>
 
