0a97663401d059e2190dddee0d2b01404b97d7d6
Kexin Liu <blue_star1022@sina.com>
2020-11-16 08:47:57 +0800

    Add captain image for the website

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,33 +11,48 @@ export default function NavBar() {
 
   const pathes = ["Home", "Shopping List", "Price Match", "My Lists"];
   return (
-    <div className ="banner">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          {pathes.map((path, i) => {
-            return (
-              <li
-                key={i}
-                className={
-                  currentPath === path.replace(/\s+/g, "").toLowerCase()
-                    ? "nav-item active"
-                    : "nav-item"
-                }
-              >
-                <Link
-                  className="nav-link"
-                  to={path.replace(/\s+/g, "").toLowerCase()}
-                >
-                  {path}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+    <div>
+      <div class="bd-example">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <img class="img-fluid" alt="GroSavvy Background" src="../images/background1.jpg" data-holder-rendered="true" />
+              <div class="carousel-caption">
+                <h1>GroSavvy</h1>
+                <p>ALWAYS LESS FOR MORE</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </nav>
+
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            {pathes.map((path, i) => {
+              return (
+                <li
+                  key={i}
+                  className={
+                    currentPath === path.replace(/\s+/g, "").toLowerCase()
+                      ? "nav-item active"
+                      : "nav-item"
+                  }
+                >
+                  <Link
+                    className="nav-link"
+                    to={path.replace(/\s+/g, "").toLowerCase()}
+                  >
+                    {path}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
     </div>
   );
-  
+
 }
